02c4b1de65217854439eba51b59eb211281840d0
ShayXS <sunxycn98@gmail.com>
2021-04-01 20:46:07 -0400

    image update
    put the main character on

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PAC-MAN")
 
 FPS = 60                                    # frame per second
+PLAYER_WIDTH, PLAYER_HEIGHT = 50, 50        # size of the player, in case we want to resize later
+
+
+BACKGROUND = pygame.image.load(os.path.join('assets','background.png'))     # background image
+WEAK_DOGE_IMAGE = pygame.image.load(os.path.join('assets','weak.png'))      # Main player image
+WEAK_DOGE = pygame.transform.scale(WEAK_DOGE_IMAGE,(PLAYER_WIDTH,PLAYER_HEIGHT))    # scale
 
 
 def draw_window():
-    WIN.fill((255,255,255))                 # to change the background color
+    #WIN.fill((255,255,255))                 # to change the background color
+    WIN.blit(WEAK_DOGE,(1,1))
+    
     pygame.display.update()
 
 
