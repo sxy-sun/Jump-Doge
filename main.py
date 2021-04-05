c76652c9339bf06bd23750a9544614de38e112df
ShayXS <sunxycn98@gmail.com>
2021-04-05 17:41:26 -0400

    Adde animation
    Doge face left Doge face right

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import os
 
 WIDTH, HEIGHT = 800, 600
 FPS = 60                                    # frame per second
-#PLAYER_WIDTH, PLAYER_HEIGHT = 50, 50        # size of the player
-#vel = 5                                     # velocity of the player
-
-#isJump = False                              # is the doge in the jump motion
-#jumpCount = 10
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("JUMP-DOGE")
@@ -22,37 +17,41 @@ class character(object):
         self.vel = 5
         self.isJump = False
         self.jumpCount = 10
+        self.left = False
+        self.right = True
         
 
 player = character(0, HEIGHT - 50, 50, 50)
 
-# background image
-BACKGROUND = pygame.transform.scale(pygame.image.load(
-    os.path.join('assets','background.png')),(400, 400))    
-
-PLAYER_IMAGE = pygame.image.load(os.path.join('assets','player.png'))      # Main player image
-PLAYER = pygame.transform.scale(PLAYER_IMAGE,(player.width,player.height))    # scale
+# Image Load
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets','background.png')),(400, 400))    
+PLAYER_IMAGE_LEFT = pygame.image.load(os.path.join('assets','playerL.png'))      # player faces left
+PLAYER_IMAGE_RIGHT = pygame.image.load(os.path.join('assets','playerR.png'))      # player faces right
+PLAYER_LEFT = pygame.transform.scale(PLAYER_IMAGE_LEFT,(player.width,player.height))    # scale
+PLAYER_RIGHT = pygame.transform.scale(PLAYER_IMAGE_RIGHT,(player.width,player.height))    # scale
 
 
 def draw_window(player):
     WIN.fill((0, 0, 0))
     WIN.blit(BACKGROUND,(180, 100))
-    WIN.blit(PLAYER,(player.x, player.y))      # here we re-draw the thing with the pos of the rect
-    
+    if player.left:
+        WIN.blit(PLAYER_LEFT,(player.x, player.y))     
+    elif player.right:
+        WIN.blit(PLAYER_RIGHT,(player.x, player.y))     
     pygame.display.update()
-
-
-def player_movement(keys_pressed, player):          # the function to move the object
     
+    
+def player_movement(keys_pressed, player):          # the function to move the object
     if keys_pressed[pygame.K_LEFT] and player.x > player.vel:
         player.x -= player.vel
-    if keys_pressed[pygame.K_RIGHT] and player.x < WIDTH - player.width - player.vel:
+        player.left = True
+        player.right = False
+    elif keys_pressed[pygame.K_RIGHT] and player.x < WIDTH - player.width - player.vel:
         player.x += player.vel
-    '''
-    if keys_pressed[pygame.K_DOWN] and player.y < HEIGHT - PLAYER_HEIGHT - player.vel:
-        player.y += player.vel
-    '''
-    
+        player.left = False
+        player.right = True
+
+        
     # JUMP
     if not (player.isJump):
         if keys_pressed[pygame.K_UP]:
@@ -87,7 +86,6 @@ def main():
         draw_window(player)                       # draw a new object aka the player doge
     
     pygame.quit()
-    
     
     
 if __name__ == "__main__":
